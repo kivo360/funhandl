c32f8827619bad0e48f96505dea5441e2787698b
Administrator <admin@example.com>
2019-01-02 23:56:39 -0800

    Added a shell dispatcher

--- funhandler/blocks/actions/action_dispatch.py
+++ funhandler/blocks/actions/action_dispatch.py
@@ -1,33 +1,123 @@
 from funhandler.blocks import ActionBlock
 from loguru import logger
 from random import choice
+import numpy as np
+
+logger.level("STEP", no=15, color="<green>", icon="🐍")
 
 class DispatchOrder(ActionBlock):
     def __init__(self, name, **kwargs):
         super().__init__(name, "dispatchorder", **kwargs)
-        self.dispatch_type = kwargs.get("dispatch_type", "loop")    
-    
+        self.dispatch_type = kwargs.get("dispatch_type", "loop")
+        self.required_objects = {}
+        self.add_requirements("portfolio")
+        self.add_required(**kwargs)
+
+
+
     def __call__(self, **kwargs):
-        pass
-    
+        self.add_required(**kwargs)
+        
+    def add_required(self, **kwargs):
+        portfolio = kwargs.get("portfolio")
+        rkeys = self.required_objects.keys()
+        if portfolio is not None:
+            logger.opt(ansi=True).info("Adding a <r>portfolio</r>")
+            if "portfolio" not in rkeys:
+                self.required_objects["portfolio"] = portfolio
+
     def add_requirements(self, *args):
-        return super().add_requirements(*args)
+        # Maybe push requirement of having a portfolio object included
+        for a in args:
+            if isinstance(a, str):
+                self.required.append(a)
     
     def check_required(self):
-        logger.success(self.required)
+        rkeys = self.required_objects.keys()
+        for _ in self.required:
+            if _ not in rkeys:
+                raise AttributeError("{} not added".format(_))
     
     def action_filter(self):
         logger.debug("Action Filter")
 
     def reset(self):
-        logger.debug("Reset Dispatcher")
+        self.check_required()
+        # logger.debug("Reset Dispatcher")
+
+    def error_responses(self, _type, **kwargs):
+        if _type == "missing":
+            name = kwargs.get("vname")
+            message = "{} variable not added".format(name)
+            raise AttributeError(message)
 
     def step(self, action, **kwargs):
-        logger.debug("Step through dispatcher {action}, {}", self.dispatch_type, action=action)
+        pct = kwargs.get("pct")
+        base = kwargs.get("base")
+        trade = kwargs.get("trade")
+        exchange = kwargs.get("exchange")
 
+        if pct is None:
+            self.error_responses("missing", vname="Pct")
+        if base is None:
+            self.error_responses("missing", vname="Base")
+        if trade is None:
+            self.error_responses("missing", vname="Trade")
+
+        if exchange is None:
+            self.error_responses("missing", vname="Exchange")
+
+
+        msg = """<y>Action:</y>{action}\t<y>Pair:</y>{base}-{trade}-{exchange}""".replace("\n", " ")
+
+        logger.opt(ansi=True).debug(
+            msg, 
+            action=action, 
+            base=base, 
+            trade=trade, 
+            exchange=exchange
+        )
+        if self.dispatch_type == "loop":
+            logger.log("STEP", "Send orders to function (celery)")
+            logger.log("STEP", "\t- Get all users that need to be ordered in")
+            logger.log("STEP", "\t- Check if user's are enabled")
+            logger.log("STEP", "\t- Send to order server")
+            logger.log("STEP", "\t\t- Dispatch to monitor system (monitor feedback loop)")
+            logger.log("STEP", "Send orders to local portfolio")
+            logger.log("STEP", "\t- Make changes to local portfolio (accessible using ray)")
+            logger.log("STEP", "\t- return the state of the local portfolio")
+            portfolio = self.required_objects["portfolio"]
+            balance, shares, holdings = portfolio.step(action)
+            logger.debug((balance, shares, holdings))
+
+
+
+class Portfolio(object):
+    def __init__(self):
+        pass
+    
+    def step(self, action):
+        logger.opt(ansi=True).debug("<m>{}</m>", action)
+        balance = 1000
+        shares = {}
+        holdings = {}
+        return balance, shares, holdings
+    
 
 if __name__ == "__main__":
     dispatch = DispatchOrder("Live Dispatcher")
+    dispatch(portfolio=Portfolio())
     dispatch.reset()
-    for i in range(10):
-        dispatch.step(choice(["buy", "sell"]))
+    for i in range(1):
+        order_choice = choice(["buy", "sell", "hold"])
+        order_pct = np.random.beta(1,2)
+        base="USDT"
+        trade=choice(["ETH", "BTC", "ETC", "DASH"])
+        exchange=choice(["binance", "bittrex", "huboi"])
+        dispatch.step(
+            order_choice, 
+            pct=order_pct, 
+            base=base, 
+            trade=trade, 
+            exchange=exchange
+        )
